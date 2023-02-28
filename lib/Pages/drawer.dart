0eda09f722cb92cb6011a6ce9fd74205543e8f56
Taufiqul Hafizh <91123146+Thaofoo@users.noreply.github.com>
2023-03-01 05:24:00 +0700

    Design update

--- lib/Pages/drawer.dart
+++ lib/Pages/drawer.dart
@@ -35,88 +35,107 @@ class DrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Color.fromARGB(255, 26, 26, 26),
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          _drawerHeader(
-          
-          ),
-          Divider(height: 25, thickness: 3, color: Colors.white),
-          _drawerItem(
-            icon: Icons.person,
-            text: 'My Profile',
-            onTap: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Profile()),
-              );
-            },),
-          _drawerItem(
-            icon: Icons.list_alt,
-            text: 'Stream Plan',
-            onTap: () => print('Tap Shared menu')),
-          _drawerItem(
-            icon: Icons.access_time,
-            text: 'Recent',
-            onTap: () => print('Tap Recent menu')),
-          Divider(height: 25, thickness: 1, color: Colors.grey, indent: 20, endIndent: 20,),
-          Padding(
-            padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
-            child: Text("Settings",
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.white,
-                        )),
-          ),
-          _drawerItem(
-            icon: Icons.settings,
-            text: 'Settings',
-            onTap: (){
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Settings()),
-                );
-              },
-            ),
-          // _drawerItem(
-          //   // icon: Icons.perm_device_info,
-          //   icon: Icons.privacy_tip_outlined,
-          //   text: 'About us',
-          //   onTap: () => print('Tap Family menu')
-          // ),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [
+                0.1,
+                0.25,
+                0.6,
+                0.9,
+              ],
+              colors: [
+                Color.fromARGB(255, 98, 48, 8),
+                Color.fromARGB(255, 73, 42, 18),
+                Color.fromARGB(255, 44, 39, 35),
+                Color.fromARGB(255, 41, 41, 41),
+              ],
+            )
+      
+      ),
+      child: Drawer(
+        backgroundColor: Color.fromARGB(91, 0, 0, 0),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            _drawerHeader(),
             _drawerItem(
-            icon: Icons.logout_outlined,
-            text: 'Log Out',
-            onTap: () {
-              showDialog(
-                context: context, 
-                builder: (context) {
-                  return AlertDialog(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                    backgroundColor: Color.fromARGB(255, 42, 42, 42),
-                    title: const Text("Sign Out", style: TextStyle(color: Colors.white),),
-                    content: const Text("Are you sure you want to sign out?", style: TextStyle(color: Colors.white),),
-                    actions: [
-                      TextButton(onPressed: (){
-                        Navigator.pop(context);
-                      }, child: const Text("Cancel", style: TextStyle(color: Colors.white),),
-                      ),
-                      TextButton(onPressed: (){
-                        Navigator.of(context).popUntil((route) => route.isFirst);
-                        Navigator.pushReplacement(
-                        context, MaterialPageRoute(builder: (BuildContext context) => Login()));
-                      }, child: const Text("Confirm", style: TextStyle(color: Colors.white),),
-                      ),
-                    
-                    ],
-                  );
-                }
+              icon: Icons.person,
+              text: 'My Profile',
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Profile()),
                 );
-            },),
-        ],
+              },),
+            _drawerItem(
+              icon: Icons.list_alt,
+              text: 'Stream Plan',
+              onTap: () => print('Tap Shared menu')),
+            _drawerItem(
+              icon: Icons.access_time,
+              text: 'Recent',
+              onTap: () => print('Tap Recent menu')),
+            Divider(height: 25, thickness: 0.75, color: Color.fromARGB(151, 79, 79, 79), indent: 20, endIndent: 20,),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
+              child: Text("Settings",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 15,
+                color: Colors.white,
+                          )),
+            ),
+            _drawerItem(
+              icon: Icons.settings,
+              text: 'Settings',
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Settings()),
+                  );
+                },
+              ),
+            // _drawerItem(
+            //   // icon: Icons.perm_device_info,
+            //   icon: Icons.privacy_tip_outlined,
+            //   text: 'About us',
+            //   onTap: () => print('Tap Family menu')
+            // ),
+              _drawerItem(
+              icon: Icons.logout_outlined,
+              text: 'Log Out',
+              onTap: () {
+                showDialog(
+                  context: context, 
+                  builder: (context) {
+                    return AlertDialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                      backgroundColor: Color.fromARGB(255, 42, 42, 42),
+                      title: const Text("Sign Out", style: TextStyle(color: Colors.white),),
+                      content: const Text("Are you sure you want to sign out?", style: TextStyle(color: Colors.white),),
+                      actions: [
+                        TextButton(onPressed: (){
+                          Navigator.pop(context);
+                        }, child: const Text("Cancel", style: TextStyle(color: Colors.white),),
+                        ),
+                        TextButton(onPressed: (){
+                          Navigator.of(context).popUntil((route) => route.isFirst);
+                          Navigator.pushReplacement(
+                          context, MaterialPageRoute(builder: (BuildContext context) => Login()));
+                        }, child: const Text("Confirm", style: TextStyle(color: Colors.white),),
+                        ),
+                      
+                      ],
+                    );
+                  }
+                  );
+              },),
+          ],
+        ),
       ),
     );
   }
@@ -124,14 +143,18 @@ class DrawerWidget extends StatelessWidget {
 
 Widget _drawerHeader() {
     return UserAccountsDrawerHeader(
-      decoration: BoxDecoration(color: Color.fromARGB(255, 26, 26, 26)),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(0, 26, 26, 26),
+        border: Border(bottom: BorderSide(color: Color.fromARGB(150, 79, 79, 79), width: 0.75))
+        ),
       currentAccountPicture: ClipOval(
              child: Image(
                  image: AssetImage('images/edo.jpg'),
                  fit: BoxFit.cover),
              ),
-      accountName: Text('Nathanael Erlando Putra', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
-      accountEmail: Text('erlando.putra.21@gmail.com', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white  ),),
+      accountName: Text('Nathanael Erlando Putra', style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white),),
+      accountEmail: Text('erlando.putra.21@gmail.com', style: TextStyle(fontWeight: FontWeight.w300, color: Color.fromARGB(255, 139, 139, 139)  ),),
+      
     );
 }
 
@@ -142,12 +165,13 @@ Widget _drawerItem(
               children: <Widget>[
                   Icon(icon, color: warnaEmas,),
                   Padding(
-                      padding: EdgeInsets.only(left: 25.0),
+                      padding: EdgeInsets.only(left: 15.0),
                       child: Text(
                             text,
                             style: TextStyle(
+                              fontSize: 14,
                               color: Colors.white,
-                              fontWeight: FontWeight.bold,
+                              fontWeight: FontWeight.w400,
                             ),
                       ),
                  ),
