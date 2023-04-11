bed619ccdbc813567f584fced32218fea98a4519
Adelnors <erlando.putra.21@gmail.com>
2023-04-11 09:47:54 +0700

    tidied up fonts

--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
 file(TO_CMAKE_PATH "D:\\flutter\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\UNAIR\\Semester 4\\P Mobile Prak\\GitHub\\geekiga" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\Nathanael Erlando Putra\\GitHub\\geekiga_mob_v2\\geekiga" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -11,14 +11,14 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
   "FLUTTER_ROOT=D:\\flutter\\flutter"
-  "PROJECT_DIR=C:\\UNAIR\\Semester 4\\P Mobile Prak\\GitHub\\geekiga"
+  "PROJECT_DIR=C:\\Nathanael Erlando Putra\\GitHub\\geekiga_mob_v2\\geekiga"
   "FLUTTER_ROOT=D:\\flutter\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\UNAIR\\Semester 4\\P Mobile Prak\\GitHub\\geekiga\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\UNAIR\\Semester 4\\P Mobile Prak\\GitHub\\geekiga"
-  "FLUTTER_TARGET=C:\\UNAIR\\Semester 4\\P Mobile Prak\\GitHub\\geekiga\\lib\\main.dart"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Nathanael Erlando Putra\\GitHub\\geekiga_mob_v2\\geekiga\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Nathanael Erlando Putra\\GitHub\\geekiga_mob_v2\\geekiga"
+  "FLUTTER_TARGET=C:\\Nathanael Erlando Putra\\GitHub\\geekiga_mob_v2\\geekiga\\lib\\main.dart"
   "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=C:\\UNAIR\\Semester 4\\P Mobile Prak\\GitHub\\geekiga\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=C:\\Nathanael Erlando Putra\\GitHub\\geekiga_mob_v2\\geekiga\\.dart_tool\\package_config.json"
 )
